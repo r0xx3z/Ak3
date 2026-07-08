31e9c462cb0fdb7f478d3173708f26859d4cf0ce
Veruz <axelgpg2290@gmail.com>
2026-07-09 05:20:24 +0700

    Add flags for Spes

--- anykernel.sh
+++ anykernel.sh
@@ -10,8 +10,8 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=sweet
-device.name2=sweetin
+device.name1=spes
+device.name2=spesn
 supported.versions=11 - 16
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -25,8 +25,8 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=/dev/block/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=0;
+BLOCK=boot;
+IS_SLOT_DEVICE=1;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
